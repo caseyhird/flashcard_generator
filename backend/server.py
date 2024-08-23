3a986e0439d7d794d41b64ade52e58de87fd719a
Casey Hird <hirdcasey@gmail.com>
2024-08-23 15:14:10 -0700

    update server host to 0.0.0.0

--- backend/server.py
+++ backend/server.py
@@ -27,4 +27,4 @@ async def create_upload_file(file: UploadFile = File(...)) -> List[FlashCard]:
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app, port = 80)
+    uvicorn.run(app, host="0.0.0.0", port=80)
