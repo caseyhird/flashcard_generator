d82fff5b6c0086c09cfe6c1bacada623999c1d4d
Casey Hird <hirdcasey@gmail.com>
2024-08-26 18:37:13 -0700

    fix cors origin typo

--- backend/server.py
+++ backend/server.py
@@ -17,7 +17,7 @@ app = FastAPI()
 
 origins = [
     "http://localhost:5173",
-    "https://flashcard-frontend-rosy.vercel.app/"
+    "https://flashcard-frontend-rosy.vercel.app"
 ]
 
 app.add_middleware(
